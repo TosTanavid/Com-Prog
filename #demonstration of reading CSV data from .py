dbe441d6bd421aea963dd039aa08d8c70a8f06ce
TosTanavid <159753397+TosTanavid@users.noreply.github.com>
2024-03-18 14:40:04 +0700

    update

---
+++ #demonstration of reading CSV data from .py
@@ -0,0 +1,12 @@
+#demonstration of reading CSV data from a file into data frames and merging them
+pip3 install pandas
+import pandas as pd
+
+persons = pd.read_csv("data.csv")
+pets = pd.read_csv("pets.csv")
+
+ppets = persons.merge(pets)
+print(ppets)
+
+ppets = persons.merge(pets, how="outer")
+print(ppets)
